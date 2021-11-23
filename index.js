b5cf63ee9847efb5043dde35c5c515b9a61e863a
LisShamenko <shamenko.mailforunity@gmail.com>
2021-11-24 00:35:39 +0300

    setup

---
+++ index.js
@@ -0,0 +1,16 @@
+//const ExpressApp = require('./App/Express/ExpressApp');
+//console.log('--- ExpressApp = ' + ExpressApp);
+
+//// техно: postgres
+//const PostgresTechno = require('./Database/Postgres/PostgresTechno');
+//PostgresTechno(() => {
+//    console.log('--- PostgresTechno complete');
+//});
+
+//const PostgresApp = require('./Database/Postgres/PostgresApp');
+//console.log('--- PostgresApp = ' + PostgresApp);
+
+const SwaggerApp = require('./App/Swagger/SwaggerApp');
+console.log('--- SwaggerApp = ' + SwaggerApp);
+
+//const SwaggerApp = require('./test/ChaiExpectTechno');
