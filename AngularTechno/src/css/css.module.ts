d2fc8dc500c18414fe9f20d4944600225b68dbb8
LisShamenko <shamenko.mailforunity@gmail.com>
2021-11-24 10:27:58 +0300

    css: плавающие элементы, блочный контекст форматирования, CSS-сетка

--- AngularTechno/src/css/css.module.ts
+++ AngularTechno/src/css/css.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { CSSBaseComponent } from "./components/css_base.component";
-import { RelativeUnitsComponent } from "./components/relative_units.component";
+import { CSSBaseComponent } from "./components/css_base/css_base.component";
+import { RelativeUnitsComponent } from "./components/relative_units/relative_units.component";
+import { FloatElementsComponent } from "./components/float_elements/float_elements.component";
 
 @NgModule({
     imports: [
         BrowserModule, BrowserAnimationsModule, FormsModule, ReactiveFormsModule,
     ],
     declarations: [
-        CSSBaseComponent, RelativeUnitsComponent,
+        CSSBaseComponent, RelativeUnitsComponent, 
+        FloatElementsComponent
     ],
     bootstrap: [
-        CSSBaseComponent, RelativeUnitsComponent,
+        CSSBaseComponent, RelativeUnitsComponent, 
+        FloatElementsComponent
     ],
 
 })
