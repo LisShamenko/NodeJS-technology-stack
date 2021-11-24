f7083f3b8e0cdaef6066d94dfa32cd06bae7bc54
LisShamenko <shamenko.mailforunity@gmail.com>
2021-11-24 10:26:32 +0300

    css: относительные единицы

--- AngularTechno/src/css/css.module.ts
+++ AngularTechno/src/css/css.module.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { CSSBaseComponent } from "./components/css_base.component";
+import { RelativeUnitsComponent } from "./components/relative_units.component";
 
 @NgModule({
     imports: [
         BrowserModule, BrowserAnimationsModule, FormsModule, ReactiveFormsModule,
     ],
     declarations: [
-        CSSBaseComponent,
+        CSSBaseComponent, RelativeUnitsComponent,
     ],
     bootstrap: [
-        CSSBaseComponent,
+        CSSBaseComponent, RelativeUnitsComponent,
     ],
 
 })
