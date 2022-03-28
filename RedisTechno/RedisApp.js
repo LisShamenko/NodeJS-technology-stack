5e32c5a21ce348f6043a004f941674f73996c8cb
LisShamenko <shamenko.mailforunity@gmail.com>
2022-03-28 11:47:19 +0300

    redis: index

---
+++ RedisTechno/RedisApp.js
@@ -0,0 +1,28 @@
+
+module.exports = (chapters, examples) => {
+
+    const isChapter = (c) => {
+        return (chapters.length === 0 || chapters.find(i => i === c));
+    }
+
+    const isExample = (e) => {
+        return (examples.find(i => i === e));
+    }
+
+    // ошибка:
+    //      ReferenceError: queueMicrotask is not defined
+    // решение:
+    //      добавить в файл './node_modules/@node-redis/client/dist/lib/client/socket.js' 
+    //      следующее объявление из пакета 'queue-microtask':
+    //          const queueMicrotask = require('queue-microtask');
+
+    // ошибка:
+    //      Error: connect ECONNREFUSED 127.0.0.1: 6379
+    // решение:
+    //      lis@lis-vb:~$ cd ./redis/redis-6.2.6
+    //      lis@lis-vb:~/redis/redis-6.2.6$ redis-server
+
+    // --------------- Programming with Redis.
+    if (isChapter(1)) {
+    }
+}
